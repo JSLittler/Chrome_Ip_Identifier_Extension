a96f02405e039cbf935b0f3dbe80de41ee763b5b
KarlNewark <karlnewark@msn.com>
2020-07-13 14:50:13 +0100

    updated start script

--- dummy_site/index.js
+++ dummy_site/index.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World! <p>192.168.44.1</p> <h1>192.168.1.1</h1> 192.168.1.26 <h2>sdvvf192.166.3.4</h2>'))
+app.get('/', (req, res) => res.send('Hello World!<p>52.16.89.14</p> <p>172.217.7.174</p> <h1>205.251.242.103</h1> <p>17.172.224.47</p> <h2>sdvvf192.166.3.4</h2>'))
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
--- splunkIpScript.js
+++ splunkIpScript.js
@@ -1,26 +1,139 @@
-window.alert('oeirhgoiehrgoih');
-console.log('here');
+console.log('now');
+let ipDetailsArray = [];
 
-const ipArray = []
-const isIp = (myString) => {
-    const r = /\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b/
-    let ip;
-    myString.match(r) ? ip = myString.match(r)[0] : ip = '';
-    if (ip) {
-        return ip;
+
+runDecorator = () => {
+
+    console.log('runDecorator');
+
+    let pageNodes = [...document.body.childNodes];
+    const getChildNodes = (node) => {
+        if (!!node.childNodes.length) {
+            pageNodes = [...pageNodes, ...node.childNodes];
+        }
+    };
+    const populatePageNodes = () => {
+        for (let i = 0; i < pageNodes.length; i++) {
+            getChildNodes(pageNodes[i])
+        };
     }
-};
-const getBodyArray = () => {
-    const body = document.body.textContent;
-    const array = body.split(" ");
-    array.filter(Boolean);
-    return array.filter(s => s.length > 6);
-};
-const textArray = getBodyArray();
-const fillIpArray = (string) => {
-    const ip = isIp(string);
-    ip && ipArray.push(ip);
-}
-textArray.map(s => fillIpArray(s));
-console.log([...new Set(ipArray)]);
+    const getNodeText = () => {
+        return nodeText = pageNodes.map(v => v.nodeValue);
+    };
+    const getIpsOnPage = () => {
+        populatePageNodes();
+        const array = getNodeText().filter(e => { return /\S/.test(e); });
+        const cleanArray = array.filter(Boolean);
+        return cleanArray.filter(e => { return e.search(/\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b/) > -1; });
+    };
+    const ipArray = getIpsOnPage();
 
+    console.log(ipArray);
+
+    if (ipArray.length < 1) {
+        return;
+    }
+
+    
+
+    ipArray.forEach((ip, index) => {
+        console.log('request');
+        fetch(`https://ipapi.co/${ip}/json/`).then(
+            response => {
+                response.json().then(
+                    data => {
+                        console.log('response');
+                        ipDetailsArray.push(data);
+                        if (index == ipArray.length - 1) {
+                            pageIpDecoration();
+                        }
+                    });
+            }
+        );
+    })
+
+    pageIpDecoration = () => {
+
+        ipDetailsArray.forEach(ipDetails => {
+            const newElement = document.createElement("div")
+            newElement.classList = "special-ip"
+            newElement.style = "color : green";
+            newElement.innerText = ipDetails.ip + " " + ipDetails.country_code;
+
+            const thisNode = pageNodes.find(node => node.nodeValue == ipDetails.ip);
+
+            thisNode.replaceWith(newElement);
+
+        })
+
+
+
+        console.log(ipDetailsArray)
+
+    };
+
+};
+
+// window.addEventListener('DOMContentLoaded', runDecorator);
+
+// // document.addEventListener('readystatechange', () => {    
+// //     if (document.readyState == 'complete') runDecorator();
+// //   });
+
+// window.addEventListener("load", runDecorator);
+
+//  window.onload = () => {
+//     let newBody = document.createElement("body")
+//     newBody.onload="() => {runDecorator()}"
+//     document.querySelector('html').appendChild(newBody);
+
+
+//  };
+
+const timer = window.setInterval(() => {
+
+    if (ipDetailsArray.length) {
+        clearInterval(timer);
+        return;
+    }
+
+    if (document.readyState === 'complete') {
+        runDecorator();
+    }
+}, 1000)
+
+timer();
+
+//document.addEventListener('change', runDecorator);
+
+// // Select the node that will be observed for mutations
+// const targetNode = document.getElement('body');
+
+// // Options for the observer (which mutations to observe)
+// const config = { attributes: true, childList: true, subtree: true };
+
+// // Callback function to execute when mutations are observed
+// const callback = function(mutationsList, observer) {
+//     // Use traditional 'for loops' for IE 11
+//     for(let mutation of mutationsList) {
+//         if (mutation.type === 'childList') {
+//             console.log('A child node has been added or removed.');
+//         }
+//         else if (mutation.type === 'attributes') {
+//             console.log('The ' + mutation.attributeName + ' attribute was modified.');
+//         }
+
+//         runDecorator();
+//     }
+// };
+
+// // Create an observer instance linked to the callback function
+// const observer = new MutationObserver(callback);
+
+// // Start observing the target node for configured mutations
+// observer.observe(targetNode, config);
+
+// // // Later, you can stop observing
+// // observer.disconnect();
+
+//https://splunk.meer-spacestation.co.uk/en-US/app/Home/report?s=%2FservicesNS%2Fnobody%2FHome%2Fsaved%2Fsearches%2FIP%2520-%2520Top%2520requests%2520in%2520IIS%2520logs&sid=_a2FybC5uZXdhcmtAYmdsZ3JvdXAuY28udWs_ZHVuY2FuLnNhbXBsZUBiZ2xncm91cC5jby51aw__Home__RMD56b0f9e1951b21b81_at_1594133413_90601&display.page.search.mode=verbose&dispatch.sample_ratio=1&q=search%20index%3Daws_home_prod%20sourcetype%3Diis%20x_forward_for_client_ip%3D*%20%7C%20timechart%20count%20by%20x_forward_for_client_ip%20limit%3D10%20useother%3Df&earliest=-60m%40m&latest=now
