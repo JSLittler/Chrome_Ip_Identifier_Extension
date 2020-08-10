e4ff80da763c32a101994d70d3c7dc5c94f01a6a
John Littler <johnlittler@BGL-MAC-020424.bgl.financial.local>
2020-08-10 23:39:58 +0100

    modules : tidying up.

--- components/runDecorator.js
+++ components/runDecorator.js
@@ -7,25 +7,25 @@ const runDecorator = () => {
     let ipArray = [];
     let pageNodes = [...document.body.childNodes];
     let ipStore = {
-    ipStoreArray: [],
-    addIpDetailsToStore : (ipDetails) => {
-        ipStore.ipStoreArray.push(ipDetails);
-        window.localStorage.setItem('ipStorage', JSON.stringify(ipStore.ipStoreArray) );
-    },
-    getIpDetails : (ip) => {
-        return ipStore.ipStoreArray.filter((e) => {return e.ip == ip})
-    },
-    getAllIpDetails : () => {
-        return ipStore.ipStoreArray;
-    },
-    setupStoreArray : () => {
-        let fromStorage = window.localStorage.getItem('ipStorage');
-        if(fromStorage){
-            fromStorage = JSON.parse(fromStorage);
-            ipStore.ipStoreArray = fromStorage;
+        ipStoreArray: [],
+        addIpDetailsToStore : (ipDetails) => {
+            ipStore.ipStoreArray.push(ipDetails);
+            window.localStorage.setItem('ipStorage', JSON.stringify(ipStore.ipStoreArray) );
+        },
+        getIpDetails : (ip) => {
+            return ipStore.ipStoreArray.filter((e) => {return e.ip == ip})
+        },
+        getAllIpDetails : () => {
+            return ipStore.ipStoreArray;
+        },
+        setupStoreArray : () => {
+            let fromStorage = window.localStorage.getItem('ipStorage');
+            if(fromStorage){
+                fromStorage = JSON.parse(fromStorage);
+                ipStore.ipStoreArray = fromStorage;
+            }
         }
-    }
-};
+    }; 
 
     const decoratePage = async () => {
         ipStore.setupStoreArray();
