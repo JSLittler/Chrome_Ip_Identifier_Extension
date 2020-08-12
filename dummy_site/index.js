af9842966d7b8d97867e575a1a73e9303c9a5232
KarlNewark <karlnewark@msn.com>
2020-08-12 16:32:41 +0100

    early work

--- dummy_site/index.js
+++ dummy_site/index.js
@@ -1,7 +1,15 @@
 const express = require('express')
 const app = express()
 const port = 3000
+var path = require('path');
 
-app.get('/', (req, res) => res.send('<body>Hello World!<p>52.16.89.14</p> <p>172.217.7.174</p> <h1>205.251.242.103</h1> <p class="change-me">17.172.224.47</p> <h2>sdvvf192.166.3.4</h2></body>'))
+app.get('/', (req, res) => 
+
+
+res.sendFile(path.join(__dirname + '/index.html'))
+
+)
+
+
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
