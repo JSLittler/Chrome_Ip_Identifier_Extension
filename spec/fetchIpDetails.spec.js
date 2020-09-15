78121d073fe13e2f1a5c0c8d198aeb5395a97776
KarlNewark <karlnewark@msn.com>
2020-09-15 14:46:46 +0100

    removing unnecessary mock

--- spec/fetchIpDetails.spec.js
+++ spec/fetchIpDetails.spec.js
@@ -1,12 +1,6 @@
 import fetchIpDetails from '../components/fetchIpDetails.js';
 
 describe("Fetch Ip Details", function() {
-  global.fetch = jest.fn(() =>
-    Promise.resolve({
-      json: () => Promise.resolve({ rates: { CAD: 1.42 } }),
-    })
-  );
-
   it("returns an array", async () => {
     const data = await fetchIpDetails([], []);
     
