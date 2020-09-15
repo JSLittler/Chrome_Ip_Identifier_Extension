aa2a5ed2fc8d7d35f1df15b349210cc09caaa647
John Littler <johnlittler@BGL-MAC-020424.bgl.financial.local>
2020-09-15 13:37:14 +0100

    Test : Fixed mocks config

--- jest.config.mjs
+++ jest.config.mjs
@@ -125,7 +125,7 @@ export default {
   // The paths to modules that run some code to configure or set up the testing environment before each test
   // setupFiles: [],
   setupFiles: [
-    "./__mocks__/client.js"
+    "./mocks/client.js"
   ],
 
   // A list of paths to modules that run some code to configure or set up the testing framework before each test
--- spec/testtests.spec.js
+++
@@ -1,8 +0,0 @@
-// import double from '../components/testComponent.js';
-
-// describe("Test Suite", function() {
-//     it("test spec", function() {
-//         expect( double(1) ).toEqual(2);
-//     }); 
-// });
-
